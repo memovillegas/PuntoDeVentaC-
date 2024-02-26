e68a4973f90cae5e1bbe6cb2608e785c4f6048b3
memovillegas <apoloniomemo@gmail.com>
2024-02-25 23:10:04 -0600

    Primer commit, diseño login

---
+++ PuntoDeVenta}/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PuntoDeVenta_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_contrasena = new System.Windows.Forms.TextBox();
+            this.btn_Entrar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txt_usuario
+            // 
+            resources.ApplyResources(this.txt_usuario, "txt_usuario");
+            this.txt_usuario.Name = "txt_usuario";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt_contrasena
+            // 
+            resources.ApplyResources(this.txt_contrasena, "txt_contrasena");
+            this.txt_contrasena.Name = "txt_contrasena";
+            this.txt_contrasena.UseSystemPasswordChar = true;
+            // 
+            // btn_Entrar
+            // 
+            resources.ApplyResources(this.btn_Entrar, "btn_Entrar");
+            this.btn_Entrar.Name = "btn_Entrar";
+            this.btn_Entrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Salir
+            // 
+            resources.ApplyResources(this.btn_Salir, "btn_Salir");
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Entrar);
+            this.Controls.Add(this.txt_contrasena);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_contrasena;
+        private System.Windows.Forms.Button btn_Entrar;
+        private System.Windows.Forms.Button btn_Salir;
+    }
+}
+
